3858cb1ed4e632fc9a5983bf983549f7771859cf
kihBootCamp <kimberleyheuer@hotmail.com>
2020-11-16 14:17:51 -0600

    test push

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
